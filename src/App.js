c34841b3d86f8d288140ad606e709a9e9cb880d2
nonamedm <nonamedm@naver.com>
2023-07-24 18:07:16 +0900

    최종변경

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
+/* eslint-disable */
+
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
 function App() {
 
-  let post = '강남 우동 맛집'; // 자료 잠깐 저장할 때는 변수에 저장 
-                              // let var const
-  let [글제목,b] = useState('남자 코트 추천');
+  let post = '강남 우동 맛집'; // 자료 잠깐 저장할 때는 변수에 저장 let var const
+  let [글제목,글제목변경] = useState(['남자 코트 추천','강남 우동맛집','파이썬독학']);
+  let [logo,setLogo] = useState('ReactBlog');
+  let [따봉,따봉변경] = useState(0);
+
+  function 함수() {
+    {따봉+1};
+  }
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>블로그제작중</h4>
+        <h4>ReactBlog</h4>
+      </div>
+
+      <button onClick={() => {
+          let copy = [...글제목];
+          copy[0] = '여자 코트 추천';
+          글제목변경(copy);
+          }}>글수정</button>
+      <div className="list">
+        <h4>{글제목[0]}
+        <span onClick={() => {따봉변경(따봉+1)}}>👍🏻</span> {따봉}
+        </h4>
+        <p>2월 17일 발행</p>
+      </div>
+
+
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{글제목}</h4>
+        <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
     </div>
